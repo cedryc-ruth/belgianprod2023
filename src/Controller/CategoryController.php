082c158c94b8c64b831ca8db40f8616becf5bd90
Bob Sull <bob@sull.com>
2023-12-13 15:44:03 +0100

    Formulaire d'insertion

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,6 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Form\CategoryType;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class CategoryController extends AbstractController
 {
@@ -24,7 +27,7 @@ class CategoryController extends AbstractController
         ]);
     }
 
-    #[Route('/category/{id}', name: 'app_category_show')]
+    #[Route('/category/{id}', name: 'app_category_show', requirements : ['id' => "\d+"])]
     public function show(EntityManagerInterface $em, int $id): Response
     {
         $repository = $em->getRepository(Category::class);
@@ -33,6 +36,37 @@ class CategoryController extends AbstractController
         return $this->render('category/show.html.twig', [
             'title' => 'Fiche catégorie',
             'category' => $category,
+        ]);
+    }
+
+    #[Route('/category/new', name: 'app_category_new')]
+    public function new(EntityManagerInterface $em, Request $request): Response
+    {
+        //Créer l'entité
+        $category = new Category();
+
+        //Créer le formulaire
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        //Traiter le formulaire
+        if($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();
+
+            $em->persist($category);
+
+            $em->flush();
+
+            if($category->getId()) {
+                return $this->redirectToRoute('app_category');
+            } 
+        }
+
+        //Afficher le formulaire
+        return $this->render('category/new.html.twig', [
+            'form' => $form,
+            'title' => "Ajout d'une catégorie",
         ]);
     }
 }
